0e4c66e0bb8ba2f1b2b69ae0315838656c69e381
Gram <master_fess@mail.ru>
2018-03-01 18:46:34 +0500

    FIX settings manager

--- djbrut/settings.py
+++ djbrut/settings.py
@@ -1,11 +1,15 @@
 # django
-from django.conf import LazySettings
+from django.conf import settings as _settings
 
 from . import default_settings
 
 
-class Settings(LazySettings):
+class Settings(object):
     def __getattr__(self, name):
+        try:
+            return getattr(_settings, name)
+        except AttributeError:
+            pass
         return getattr(default_settings, name)
 
 
